06f23bc9224a447cd5e680cb62f1aa4ec6313892
soohyun <254634043+hardthink26@users.noreply.github.com>
2026-08-13 16:21:01 +0900

    add feature/userLogin

--- app/models.py
+++ app/models.py
@@ -1,4 +1,8 @@
 from . import db 
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin 
+from . import login_manager
+from flask_login import login_required 
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -10,7 +14,7 @@ class Role(db.Model):
 
     users = db.relationship('User', backref='role', lazy='dynamic')
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True )
@@ -21,6 +25,18 @@ class User(db.Model):
 
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     problems = db.relationship('Problem', backref='user', lazy='dynamic')
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute.')
+
+    @password.setter 
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Problem(db.Model):
     __tablename__ = 'problems'
@@ -32,3 +48,9 @@ class Problem(db.Model):
 
     def __repr__(self):
         return '<Problem %r>' %self.name
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
