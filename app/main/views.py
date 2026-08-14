a6f58c5aa977e6a65540fe9d558cc49df5fd4dee
soohyun <254634043+hardthink26@users.noreply.github.com>
2026-08-14 14:35:31 +0900

    feat: add navigation to user page

--- app/main/views.py
+++ app/main/views.py
@@ -9,22 +9,22 @@ from flask_login import current_user
 
 @main.route('/')
 def index():
-    return render_template('index.html', name =current_user.username, current_time=datetime.utcnow())
+    return render_template('index.html', current_time=datetime.utcnow())
 
-@main.route('/user/<name>', methods=['GET', 'POST'])
-def user_page(name):
+@main.route('/user', methods=['GET', 'POST'])
+def user_page():
     review = ReviewForm()
-    user = User.query.filter_by(username = name).first()
+    user = current_user
 
     if review.validate_on_submit():
         problem = Problem(name = review.problem.data, date = review.date.data, 
                         importance = review.importance.data, user = user)
         db.session.add(problem)
         db.session.commit()
-        return redirect(url_for('.user_page', name=name))
+        return redirect(url_for('.user_page'))
 
     problems = user.problems.all()
 
-    return render_template('user_page.html',review=review, name=name , problems=problems, 
+    return render_template('user_page.html',review=review, problems=problems, 
                            current_time=datetime.utcnow())
     
