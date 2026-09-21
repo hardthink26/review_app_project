11b5713a75cbde3c5de7c9c3fa02ce5354a052b8
soohyun <254634043+hardthink26@users.noreply.github.com>
2026-09-21 16:14:04 +0900

    feature/userCommentModel

--- app/models.py
+++ app/models.py
@@ -111,6 +111,7 @@ class User(UserMixin, db.Model):
                                backref=db.backref('followed', lazy='joined'), 
                                lazy='dynamic',
                                cascade='all, delete-orphan') 
+    comments = db.relationship('Comment', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % self.username 
@@ -225,6 +226,7 @@ class Post(db.Model):
     body_html = db.Column(db.Text) 
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comments = db.relationship('Comment', backref='post', lazy='dynamic') 
     @staticmethod 
     def on_changed_body(target, value, oldvalue, initiator): 
         allowed_tags =set(['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em',
@@ -236,3 +238,25 @@ event.listen(Post.body, 'set', Post.on_changed_body)
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
+class Comment(db.Model): 
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True) 
+    body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow) 
+    disabled = db.Column(db.Boolean) 
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags =set(['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em',
+                                   'i', 'li', 'ol', 'pre', 'strong', 'ul', 'h1', 'h2', 'h3', 'p']) 
+        target.body_html = nh3.clean(markdown(value, extensions=['pymdownx.magiclink'],                                
+                                     output_format='html'),tags=allowed_tags) 
+        
+
+
+event.listen(Comment.body, 'set', Comment.on_changed_body)
